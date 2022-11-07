b2d40f883787529cf05fdcc339b8eef2ac1b2f30
Donizete Vida <donizetevida1998@outlook.com>
2022-11-07 05:36:57 -0300

    At least it compile

--- src/com/venturus/minimal/binder/MyService.kt
+++ src/com/venturus/minimal/binder/MyService.kt
@@ -3,9 +3,16 @@ package src.com.venturus.minimal.binder
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.venturus.minimal.binder.IMyService
 
 class MyService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
+    }
+
+    class IMyServiceImpl : IMyService.Stub() {
+        override fun showAToast() {
+
+        }
     }
 }
