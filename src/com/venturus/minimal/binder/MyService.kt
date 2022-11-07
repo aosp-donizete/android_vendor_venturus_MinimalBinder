b25cae3e2f44ce24a5d4cc92551507d985a33921
Donizete Vida <donizetevida1998@outlook.com>
2022-11-07 05:25:12 -0300

    Basic Android service

---
+++ src/com/venturus/minimal/binder/MyService.kt
@@ -0,0 +1,11 @@
+package src.com.venturus.minimal.binder
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
